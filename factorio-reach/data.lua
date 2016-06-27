a86ab81121fc43eed3e7a4989e218ce8bae09f96
Sam <cplsyx@gmail.com>
2016-06-28 00:32:49 +0100

    Adding ability to mine, chop trees and discard items

--- factorio-reach/data.lua
+++ factorio-reach/data.lua
@@ -1,2 +1,4 @@
 data.raw["player"]["player"].build_distance = 10000
 data.raw["player"]["player"].reach_distance = 10000
+data.raw["player"]["player"].reach_resource_distance = 10000
+data.raw["player"]["player"].drop_item_distance = 10000
