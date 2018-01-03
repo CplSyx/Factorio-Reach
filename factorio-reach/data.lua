8699ec08d18a6bdaddb372777adb97665b711126
Sam <cplsyx@gmail.com>
2018-01-03 22:54:00 +0000

    Revert 7a1b48a, misread the API

--- factorio-reach/data.lua
+++ factorio-reach/data.lua
@@ -1,4 +1,4 @@
 data.raw["player"]["player"].build_distance = 10000
 data.raw["player"]["player"].reach_distance = 10000
-data.raw["player"]["player"].resource_reach_distance = 10000
+data.raw["player"]["player"].reach_resource_distance = 10000
 data.raw["player"]["player"].drop_item_distance = 10000
